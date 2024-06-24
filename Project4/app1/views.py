79e83d2e759c6536c2c881bc63c5d13e63cbd0de
Soumyanaik0311 <soumyasonunaik.rkl@gmail.com>
2024-06-25 00:02:06 +0530

    first commit

--- Project4/app1/views.py
+++ Project4/app1/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+# from django.http import HttpResponse
+# def soumya(request):
+#     return HttpResponse("<h1><marquee>hii, I am soumya here</h1></marquee>")
+
+# def nilesh(request):
+#     return HttpResponse("<h1><marquee>hii, I am nilesh Here</h1></marquee>")
+
 def soumya(request):
-    return HttpResponse("<h1><marquee>hii, I am soumya here</h1></marquee>")
+    return render(request, 'soumya.html')
 
 def nilesh(request):
-    return HttpResponse("<h1><marquee>hii, I am nilesh Here</h1></marquee>")
+    return render(request, 'nilesh.html')
--- Project4/app2/views.py
+++ Project4/app2/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+# from django.http import HttpResponse
+# def virat_kohli(request):
+#     return HttpResponse("<h1><marquee>Virat Kohli is best batsman</h1></marquee>")
+
+# def dhoni(request):
+#     return HttpResponse("<h1><marquee>dhoni is best wicket-keeper</h1></marquee>")
+
+
 def virat_kohli(request):
-    return HttpResponse("<h1><marquee>Virat Kohli is best batsman</h1></marquee>")
+    return render(request, 'virat_kohli.html')
 
 def dhoni(request):
-    return HttpResponse("<h1><marquee>dhoni is best wicket-keeper</h1></marquee>")
+    return render(request, 'dhoni.html')
+
